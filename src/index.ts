6e69125d7f6d8cb6a886945ac858ad5bd816ce9b
Канат Колбеков <kanat.kolbekov@smartcom.software>
2024-10-23 00:20:08 +0300

    Настройка сборки для экспорта

---
+++ src/index.ts
@@ -0,0 +1,1 @@
+export { default as PacificGeoLayout } from "@/lib-components/layouts/PacificGeoLayout.vue";
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,27 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import { resolve } from 'path';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-})
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, 'src'),
+    },
+  },
+  build: {
+    lib: {
+      entry: './src/index.ts',
+      name: 'PacificUI',
+      fileName: (format) => `pacific-ui.${format}.js`
+    },
+    rollupOptions: {
+      external: ['vue'],
+      output: {
+        globals: {
+          vue: 'Vue'
+        }
+      }
+    }
+  }
+});
